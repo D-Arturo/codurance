4e3fb62c854c3067277c1527f2b5475f2b888abe
D-Arturo <arturomf88@gmail.com>
2023-04-02 20:13:06 +0200

    initial commit

---
+++ src/test/java/kata/ATest.java
@@ -0,0 +1,12 @@
+package kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ATest {
+    @Test
+    public void testName() throws Exception {
+        fail("Not implemented");
+    }
+}
