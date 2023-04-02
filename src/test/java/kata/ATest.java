3eb11abbc770d794131be149c204956ffc3060b9
D-Arturo <arturomf88@gmail.com>
2023-04-02 20:20:19 +0200

    initial test

--- src/test/java/kata/ATest.java
+++ src/test/java/kata/ATest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 public class ATest {
     @Test
-    public void testName() throws Exception {
+    public void testName() {
         fail("Not implemented");
     }
 }
